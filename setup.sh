9e98107c3f553d2e2e26f1879bef79f40a45e410
roctbb <roctbb@gmail.com>
2023-07-05 17:14:08 +0300

    add setup files

--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@ npm install
 npm run build
 flask db init
 sudo pip3 install -r requirements.txt
-sudo cp agents_forms.conf /etc/supervisor/conf.d/
-sudo cp agents_forms_nginx.conf /etc/nginx/sites-enabled/
+sudo cp science_register.conf /etc/supervisor/conf.d/
+sudo cp science_register_nginx.conf /etc/nginx/sites-enabled/
 sudo supervisorctl update
 sudo systemctl restart nginx
 sudo certbot --nginx -d research.medsenger.ru
