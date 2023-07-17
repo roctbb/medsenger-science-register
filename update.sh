1459fc69b3529f0651c4dd773abbeae99a5475ce
roctbb <roctbb@gmail.com>
2023-07-17 19:11:31 +0300

    add flask basic-auth

--- update.sh
+++ update.sh
@@ -3,6 +3,4 @@ flask db migrate
 flask db upgrade
 npm install
 sudo pip3 install -r requirements.txt
-sudo cp agents_forms.conf /etc/supervisor/conf.d/
-sudo supervisorctl update
 sudo ./restart.sh
