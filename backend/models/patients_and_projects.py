06e1e8f10094641c4e5098d6276bf5af895e6bc5
roctbb <roctbb@gmail.com>
2023-09-29 16:13:23 +0300

    fix not in group patients

--- backend/models/patients_and_projects.py
+++ backend/models/patients_and_projects.py
@@ -31,6 +31,9 @@ class Patient(db.Model):
 
     is_legacy = db.Column(db.Boolean, default=False)
 
+    def actual_submissions(self):
+        return filter(lambda s: not s.form.is_legacy, self.submissions)
+
     def as_dict(self, additional_data={}):
         description = {
             "id": self.id,
