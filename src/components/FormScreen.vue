17eb099a32b6c86af2cab5e88eebe0c7f1066b07
roctbb <roctbb@gmail.com>
2024-05-07 18:37:46 +0300

    Refine form field rendering for disabled 'radio' and 'select'
    Changed the condition for rendering the 'radio' and 'select' fields in a disabled form. Instead of simply checking if the answer is in the options list, the code now filters options and compares the selected answer, ensuring that non-predefined options are properly displayed. This enhancement improves accuracy and usability of the form.

--- src/components/FormScreen.vue
+++ src/components/FormScreen.vue
@@ -123,7 +123,7 @@
                                                        v-bind:required="field.required"
                                                        v-bind:disabled="disabled"/>
 
-                                        <div v-if="['radio', 'select'].includes(field.type) && disabled && field.params.options.filter(o => o[0] == submission.answers[part.id][group_key][field.id]).length === 0">
+                                        <div v-if="['radio', 'select'].includes(field.type) && disabled && !Object.values(field.params.options).includes(submission.answers[part.id][group_key][field.id])">
                                             <p>{{ submission.answers[part.id][group_key][field.id] }}</p>
                                         </div>
                                         <div v-else>
@@ -299,7 +299,7 @@ export default {
             const answers = {}
 
             raw_data.forEach(row => {
-                if (row.length > 8 && row[1] && row[8]) {
+                if (row.length > 8 && row[1] && row[8] !== undefined) {
                     answers[row[1].trim()] = row[8]
                 }
             })
